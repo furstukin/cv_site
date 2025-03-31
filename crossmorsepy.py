8c17bd5caa56477652493e3fe8c5a57ae957b97b
Dustin Bloomquist <dabpython@gmail.com>
2025-03-31 17:22:07 -0400

    Add updates to index.html and include morse.html in portfolio which allows users to convert text to audible morse

--- crossmorsepy.py
+++ crossmorsepy.py
@@ -7,8 +7,13 @@ class MorseAudio:
     @classmethod
     def play_audio(cls, text_to_encode: str):
         for char in text_to_encode.lower():
-            m_char = MORSE_AUDIO_DICT[char]
-            playsound(f'static/audio/morse_code/{m_char}.mp3')
-            time.sleep(0.01)
+            # Check if the character is in the dictionary
+            if char in MORSE_AUDIO_DICT:
+                m_char = MORSE_AUDIO_DICT[char]
+                playsound(f'static/audio/morse_code/{m_char}.mp3')
+                time.sleep(0.01)  # Optional delay between sounds
+            else:
+                # Skip the character if it is not in the dictionary
+                continue
 
 
--- main.py
+++ main.py
@@ -8,10 +8,13 @@ from wtforms import StringField, SubmitField
 from wtforms.fields.numeric import FloatField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
 import requests
+from crossmorsepy import MorseAudio
+from data import NATO_CODE, MORSE_CODE, BRAILLE
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+morse_audio = MorseAudio()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('CSRF_TOKEN')
@@ -19,10 +22,28 @@ Bootstrap(app)
 
 @app.route("/")
 def home():
-    try:
-        return render_template("index.html")
-    except Exception as e:
-        return f"Error: {e}"
+    return render_template("index.html")
+
+@app.route('/morse', methods=['POST', 'GET'])
+def morse():
+    nato_sentence = ''
+    morse_sentence = ''
+    braille_sentence = ''
+    if request.method == 'POST':
+        text_to_convert = request.form.get('text-to-convert')
+        morse_audio.play_audio(text_to_convert)
+        nato_text = [NATO_CODE[letter] if letter.isalpha() else letter for letter in text_to_convert.upper()]
+        morse_text = [MORSE_CODE[letter] for letter in text_to_convert.upper()]
+        braille_text = [BRAILLE[letter] for letter in text_to_convert.lower()]
+        nato_sentence = "NATO Phonetic: " + ' '.join(nato_text).strip()
+        morse_sentence = "Morse Code: " + ''.join(morse_text).strip()
+        braille_sentence = "Braille: " + ''.join(braille_text).strip()
+    return render_template(
+        'morse.html',
+        nato_sentence=nato_sentence,
+        morse_sentence=morse_sentence,
+        braille_sentence=braille_sentence
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
