0bf8486cd0d471b027bd125e5def4f8b04c68fb7
Dustin Bloomquist <dabpython@gmail.com>
2025-03-31 18:08:58 -0400

    Revert port changes as it did not help

--- main.py
+++ main.py
@@ -45,6 +45,5 @@ def morse():
         braille_sentence=braille_sentence
     )
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Default to 8080 if PORT is not set
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    app.run(debug=False, port=5002)
