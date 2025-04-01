91ea6c6ba0c3d06caba18169dc06c71984c7b5b7
Dustin Bloomquist <dabpython@gmail.com>
2025-03-31 20:02:15 -0400

    Add 16 bit versions of audio files and switch player to pygame

--- crossmorsepy.py
+++ crossmorsepy.py
@@ -1,19 +1,18 @@
-from playsound import playsound
-import time
 from data import MORSE_AUDIO_DICT
+import pygame
+import time
 
 class MorseAudio:
-
     @classmethod
     def play_audio(cls, text_to_encode: str):
+        pygame.mixer.init(frequency=22050)  # Initialize the Pygame mixer
         for char in text_to_encode.lower():
-            # Check if the character is in the dictionary
             if char in MORSE_AUDIO_DICT:
                 m_char = MORSE_AUDIO_DICT[char]
-                playsound(f'static/audio/morse_code/{m_char}.mp3')
-                time.sleep(0.01)  # Optional delay between sounds
-            else:
-                # Skip the character if it is not in the dictionary
-                continue
-
+                audio_file = f'static/audio/morse_code/32-bit/{m_char}.mp3'
+                pygame.mixer.music.load(audio_file)
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy():
+                    time.sleep(0.25)  # Wait for audio to finish playing
+            time.sleep(0.1)
 
