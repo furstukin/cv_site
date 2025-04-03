4915e8adbb9e8cc9464c57eec9ef9a569e295da3
Dustin Bloomquist <dabpython@gmail.com>
2025-04-03 16:11:21 -0400

    Update quiz type to True/False rather than Boolean

--- data.py
+++ data.py
@@ -154,5 +154,5 @@ BRAILLE = {
 
 TRIVIA_CATEGORIES = {'Any Category': 'any', 'General Knowledge': '9', 'Entertainment: Books': '10', 'Entertainment: Film': '11', 'Entertainment: Music': '12', 'Entertainment: Musicals & Theatres': '13', 'Entertainment: Television': '14', 'Entertainment: Video Games': '15', 'Entertainment: Board Games': '16', 'Science & Nature': '17', 'Science: Computers': '18', 'Science: Mathematics': '19', 'Mythology': '20', 'Sports': '21', 'Geography': '22', 'History': '23', 'Politics': '24', 'Art': '25', 'Celebrities': '26', 'Animals': '27', 'Vehicles': '28', 'Entertainment: Comics': '29', 'Science: Gadgets': '30', 'Entertainment: Japanese Anime & Manga': '31', 'Entertainment: Cartoon & Animations': '32'}
 TRIVIA_DIFFICULTY = {'Easy': 'easy', 'Medium': 'medium', 'Hard': 'hard'}
-TRIVIA_TYPE = {'Boolean': 'boolean', 'Multiple Choice': 'multiple'}
+TRIVIA_TYPE = {'True/False': 'boolean', 'Multiple Choice': 'multiple'}
 TRIVIA_QUESTION_COUNT = [10, 20, 30, 40, 50]
